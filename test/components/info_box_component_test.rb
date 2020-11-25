a9ea6dbcb4e1df1663d6dc372910b9df5ee02cf9
Nroulston <nicolas.roulston@gmail.com>
2020-11-25 10:25:16 -0700

    refactor to component to begin drying of code

---
+++ test/components/info_box_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class InfoBoxComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(InfoBoxComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
