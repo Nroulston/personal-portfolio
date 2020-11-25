e51f02c962ab7aab69877837433b284927224abe
Nroulston <nicolas.roulston@gmail.com>
2020-11-25 10:52:42 -0700

    refactor info_box_component with odd changes
    When rendering the component using content areas the formatting of the
    Javascript code got a bit weird. To get it to display correctly I had
    to pass it in as a local variable.

--- app/components/info_box_component.rb
+++ app/components/info_box_component.rb
@@ -1,2 +1,9 @@
 class InfoBoxComponent < ViewComponent::Base
+ 
+  
+  with_content_areas :title, :body
+  attr_accessor :javascript
+  def initialize(javascript:)
+    @javascript = javascript
+  end
 end
