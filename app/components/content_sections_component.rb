f1a427d20144b09d4533c0f62957781ae5ea05ff
Nroulston <nicolas.roulston@gmail.com>
2020-11-25 12:48:47 -0700

    move content to component for dynamic rendering

--- app/components/content_sections_component.rb
+++ app/components/content_sections_component.rb
@@ -1,9 +1,23 @@
 class ContentSectionsComponent < ViewComponent::Base
-  attr_accessor :info_boxes, :title
-
-  def initialize(info_boxes:, title:)
-    @info_boxes = info_boxes
+  attr_accessor  :title, :section
+  
+  def initialize(section:, title:)
+    @section= section
     @title = title
   end
 
+  def info_boxes
+    {
+      about: [{
+        javascript: "(coffee) => { return code }",
+        title: "About Me",
+        body: 'Block-scoped. Cannot be re-assigned. Not immutable.'
+        },
+        { javascript: "(coffee) => { return code }",
+        title: "const",
+        body: 'Block-scoped. Cannot be re-assigned. Not immutable.'}
+        ],
+
+    }
+  end
 end
