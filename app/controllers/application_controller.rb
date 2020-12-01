c6e2d48cbc57ed37aa61a5ccaee4594344f119fb
Nroulston <nicolas.roulston@gmail.com>
2020-11-30 22:19:56 -0700

    create new routes and views for resume and projects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   def index
     
   end
+
+  def projects
+
+  end
+
+  def resume
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "application#index"
+
+  get '/resume', to: 'application#resume'
+  get '/projects', to: 'application#projects'
+
 end
